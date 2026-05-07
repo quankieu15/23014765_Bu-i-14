d2d7e698e12279cff01d1d47782529a613077e5d
Khánh Trần <23014765st.phenikaa-uni.edu.vn>
2026-05-07 11:39:12 +0700

    buổi 15

---
+++ b14/1.7.5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Tour - Giới thiệu</title>
+</head>
+<body>
+
+    <h1>WEBSITE DU LỊCH SỐ</h1>
+
+    <?php
+        echo "<p>Chào mừng bạn đến với website du lịch số - nền tảng giúp bạn khám phá những điểm đến tuyệt vời.</p>";
+
+        echo "<p>Hotline: 0123 456 789</p>";
+
+        echo "<p>Email: dulichso@university.edu.vn</p>";
+    ?>
+
+</body>
+</html>
