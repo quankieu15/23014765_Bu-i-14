d6a4ab71b2cca9e3becfc4935cb821fea5a455b1
Khánh Trần <23014765st.phenikaa-uni.edu.vn>
2026-05-09 15:57:44 +0700

    máy của em bị lỗi mysql không tạo được database

---
+++ b17/b1.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+if (isset($_SESSION["count"])) {
+    $_SESSION["count"] = $_SESSION["count"] + 1;
+} else {
+    $_SESSION["count"] = 1;
+}
+
+echo "Bạn đã truy cập trang web";
+?>
